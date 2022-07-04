c38026ddfa1bced55c65802de70bf03dd307fecb
bvollner <64236358+bvollner@users.noreply.github.com>
2022-07-03 20:56:27 -0700

    Liquify Transition: Initial Code

---
+++ Liquify_Transition/app.js
@@ -0,0 +1,7 @@
+new hoverEffect ({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: '7th_and_union.jpg',
+    image2: 'alona.jpg',
+    displacementImage: 'heightMap.png',
+});
