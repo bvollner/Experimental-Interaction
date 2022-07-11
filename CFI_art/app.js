d64584ca155623cf4a2b6c4103ef0d46df61c2e1
bvollner <64236358+bvollner@users.noreply.github.com>
2022-07-10 17:05:42 -0700

    CFI_art final

---
+++ CFI_art/app.js
@@ -0,0 +1,7 @@
+new hoverEffect ({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.4,
+    image1: 'final1.jpg',
+    image2: 'final2.jpg',
+    displacementImage: 'heightMap.png',
+});
